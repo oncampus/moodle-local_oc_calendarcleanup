e312a7adf3ce155b0027feee3045d486c4a4df2e
Marius Rosenbaum <marius.rosenbaum@oncampus.de>
2025-11-28 13:19:53 +0000

    upped version, release and requiredversion

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_oc_calendarcleanup';
-$plugin->release = '0.1.0';
-$plugin->version = 2025020600;
-$plugin->requires = 2022112800;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '4.5.0';
+$plugin->version = 2025112800;
+$plugin->requires = 2024100707;
+$plugin->maturity = MATURITY_STABLE;
